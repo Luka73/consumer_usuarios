829800c0e2d92d32c43616b72d624deb00640e6b
Luka Fernandes <luanafernandes73@gmail.com>
2023-04-11 12:05:38 -0400

    first commit

---
+++ src/test/java/br/com/cotiinformatica/consumer_usuarios/ConsumerUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.consumer_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
